8a843e24638b3ef7d74b7459e351c7695e1cc8cd
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-10-08 23:04:58 -0300

    Gravando conversa dos usuarios

--- msg1.py
+++ msg1.py
@@ -1,20 +1,42 @@
-import streamlit as st
+from pathlib import Path
+import pickle
 
-# msg ficticia
-MSG_FICT = [{'nome_usuario': 'Adson', 
-            'conteudo': 'Olá Giselle'},
-            {'nome_usuario': 'Giselle',
-            'conteudo': 'Ola, Adson'},]
+import streamlit as st
+from unidecode import unidecode
+
+PASTA_MENSAGENS = Path(__file__).parent / 'mensagens'
+PASTA_MENSAGENS.mkdir(exist_ok=True)
+
+# ler messagens armazenadas
+def ler_mensagen_armazenadas(usuario, conversando_com):
+    nome_arquivo = nome_arquivo_armazenado(usuario, conversando_com)
+    if (PASTA_MENSAGENS / nome_arquivo).exists():
+        with open(PASTA_MENSAGENS / nome_arquivo, 'rb') as f:
+            return pickle.load(f)
+    else:    
+        return []
+
+# salvar mensagens armazenadas
+def armazena_mensagens(usuario, conversando_com, mensagens):
+    nome_arquivo = nome_arquivo_armazenado(usuario, conversando_com)
+    with open(PASTA_MENSAGENS / nome_arquivo, 'wb') as f:
+        pickle.dump(mensagens, f)
+
+# nome_arquivo_armazenado
+def nome_arquivo_armazenado(usuario, conversando_com):
+    nome_arquivo = [usuario, conversando_com]
+    nome_arquivo.sort()
+    nome_arquivo = [u.replace(' ', '_') for u in nome_arquivo]
+    nome_arquivo = [unidecode(u) for u in nome_arquivo]
+    return '&'.join(nome_arquivo).lower()
 
 def pagina_chat():
     st.title('💬 App AJG')
     st.divider()
 
-    if not 'mensagens' in st.session_state:
-        st.session_state['mensagens'] = MSG_FICT
-
-    mensagens = st.session_state['mensagens']
     usuario_logado = 'Adson'
+    conversando_com = 'Giselle'
+    mensagens = ler_mensagen_armazenadas(usuario_logado, conversando_com)
 
     for mensagem in mensagens:
         nome_usuario = 'user' if mensagem['nome_usuario'] == usuario_logado else mensagem['nome_usuario'] 
@@ -29,7 +51,7 @@ def pagina_chat():
         chat = st.chat_message('user')
         chat.markdown(nova_dic_mensagem['conteudo'])
         mensagens.append(nova_dic_mensagem)
-        st.session_state['mensagens'] = mensagens
+        armazena_mensagens(usuario_logado, conversando_com, mensagens)
 
 def main():
     pagina_chat()
